d631e8c0f743eb21d7aaa6a0d20efaf605f506a1
Ali <alihussien615@gmail.com>
2020-04-29 23:00:15 +0200

    Added 404

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,13 +4,19 @@ const recipe = require('../models/recipe');
 module.exports = function (app) {
     // GET HANDLERS
     app.get('/', async function (req, res) {
-        const recipesFromDb = await recipes(req, res);
+        const recipesFromDb = await getAllRecipes(req, res);
+        res.render('home', { data : recipesFromDb});
+    });
+
+    app.get('/recipe', async function (req, res) {
+        const recipesFromDb = await getAllRecipes(req, res);
         res.render('home', { data : recipesFromDb});
     });
 };
 
 
-async function recipes(req, res) {
+
+async function getAllRecipes(req, res) {
     var x = await recipe.getAllRecipes();
     return x;
 }
